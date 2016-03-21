9a4b6a5d6d8343ebce9f9c01d09003d5913069d2
Artsiom Sasnouski <asasnouski@exadel.com>
2016-03-21 11:26:31 +0300

    Page 124;

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,18 @@
 'use strict';
 
-angular.module('app', []);
+angular.module('app', ['ngRoute','ngSanitize','mediaPlayer', 'ui.bootstrap', '7minWorkout']).
+  config(function ($routeProvider, $locationProvider, $sceDelegateProvider) {
+    $routeProvider.when('/start', { templateUrl: 'views/start.html' })
+    .when('/workout', { templateUrl: 'views/workout.html', controller: 'WorkoutController' })
+    .when('/finish', { templateUrl: 'views/finish.html' })
+    .otherwise({ redirectTo: '/start' });
+    //$locationProvider.html5Mode({
+    //  enabled: true,
+    //  requireBase: false
+    //});
+    $sceDelegateProvider.resourceUrlWhitelist([
+      'self',
+      'http://*.youtube.com/**']);
+  });
 
 angular.module('7minWorkout', []);
